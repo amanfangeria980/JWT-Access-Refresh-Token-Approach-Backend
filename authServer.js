4961997f72d9682a9b83a4dca38cd2c0e4c01c07
Aman Fangeria <aman.fangeria@geekyants.com>
2024-09-19 13:42:08 +0530

    working code

--- authServer.js
+++ authServer.js
@@ -1,49 +1,56 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
 const jwt = require("jsonwebtoken");
-const { generateRefreshToken,generateAccessToken } = require("./utils");
+const cookieParser = require("cookie-parser");
+const app = express();
 app.use(express.json());
-const PORT=process.env.AUTH_SERVER_PORT || 4000;
+app.use(cookieParser());
+const { generateAccessToken, generateRefreshToken } = require("./utils");
 
+const PORT = process.env.AUTH_SERVER_PORT || 4000;
 let refreshTokens = [];
 
 app.post("/token", (req, res) => {
-  let refreshToken = req.headers['refreshtoken'].split(' ')[1];
-  if (refreshToken == null) return res.sendStatus(401);
-  if (!refreshTokens.includes(refreshToken)) return res.status(403).send("Wrong refresh token");
+    const refreshToken = req.cookies.refreshToken;
 
-  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-    const newRefreshToken =generateRefreshToken({name: user.name}) 
-    refreshTokens = refreshTokens.filter(token => token !== refreshToken);
-    refreshTokens.push(newRefreshToken);
-    const accessToken = generateAccessToken({ name: user.name });
-    res.json({ accessToken: accessToken, refreshToken: newRefreshToken });
-  });
-  
+    if (!refreshToken) return res.sendStatus(401);
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        const accessToken = generateAccessToken({ name: user.name });
+        const newRefreshToken = generateRefreshToken({ name: user.name });
+
+        refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+        refreshTokens.push(newRefreshToken);
+
+        res.cookie("refreshToken", newRefreshToken, {
+            httpOnly: true,
+            secure: true,
+        });
+        res.json({ accessToken });
+    });
 });
-
-
 
 app.post("/login", (req, res) => {
-  //AUTHENTICATE USER
-  const username = req.body.username;
-  const user = { name: username };
-  const accessToken = generateAccessToken(user);
-  const refreshToken = generateRefreshToken(user);
-  res.json({ accessToken: accessToken, refreshToken: refreshToken });
-  refreshTokens.push(refreshToken);
+    const username = req.body.username;
+    if (!username)
+        return res.status(400).json({ error: "Username is required" });
+    const user = { name: username };
+    const accessToken = generateAccessToken(user);
+    const refreshToken = generateRefreshToken(user);
+    refreshTokens.push(refreshToken);
+    res.cookie("refreshToken", refreshToken, { httpOnly: true, secure: true });
+    res.json({ accessToken });
 });
-
 
 app.delete("/logout", (req, res) => {
-  refreshTokens = refreshTokens.filter(token => token !== req.body.token);
-  res.sendStatus(204);
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) return res.sendStatus(401);
+    refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+    res.clearCookie("refreshToken");
+    res.sendStatus(204);
 });
 
-
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on PORT:${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Auth Server is running on PORT:${PORT}`);
 });
