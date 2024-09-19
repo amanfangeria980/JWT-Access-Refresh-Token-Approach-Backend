c39f90db28acca72476d3b38be7e48b3fcaed24d
Aman Fangeria <aman.fangeria@geekyants.com>
2024-09-19 17:53:42 +0530

    Working fine

--- authServer.js
+++ authServer.js
@@ -12,6 +12,7 @@ const PORT = process.env.AUTH_SERVER_PORT || 4000;
 
 app.post("/token", (req, res) => {
     const refreshToken = req.cookies.refreshToken;
+    console.log(refreshToken);
     if (!refreshToken) return res.sendStatus(401);
     // if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
     jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
@@ -24,7 +25,7 @@ app.post("/token", (req, res) => {
 
         res.cookie("refreshToken", newRefreshToken, {
             httpOnly: true,
-            secure: true,
+            // secure: true,
         });
         res.json({ accessToken });
     });
@@ -38,7 +39,10 @@ app.post("/login", (req, res) => {
     const accessToken = generateAccessToken(user);
     const refreshToken = generateRefreshToken(user);
     // refreshTokens.push(refreshToken);
-    res.cookie("refreshToken", refreshToken, { httpOnly: true, secure: true });
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        //  secure: true
+    });
     res.json({ accessToken });
 });
 
@@ -49,6 +53,34 @@ app.delete("/logout", (req, res) => {
     res.clearCookie("refreshToken");
     res.sendStatus(204);
 });
+
+const posts = [
+    {
+        username: "Shreya",
+        title: "Post 1",
+    },
+    {
+        username: "Chauhan",
+        title: "Post 2",
+    },
+];
+app.get("/posts", authenticateToken, (req, res) => {
+    console.log(req.name, req);
+    res.json(posts.filter((post) => post.username === req.name));
+});
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    if (token == null) {
+        return res.sendStatus(401);
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.name = user.name;
+        next();
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Auth Server is running on PORT:${PORT}`);
